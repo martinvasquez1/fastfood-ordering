5accd84e272e35ed0b4aaf974294faa21a542bcf
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-04-27 13:53:01 -0400

    feat(api): add CASL module

---
+++ apps/api/src/casl/casl-ability.factory.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import {
+  Ability,
+  AbilityBuilder,
+  AbilityClass,
+  ExtractSubjectType,
+} from '@casl/ability';
+
+import { InferSubjects } from '@casl/ability';
+import { User } from 'src/users/entities/user.entity';
+
+export enum Action {
+  Manage = 'manage',
+  Create = 'create',
+  Read = 'read',
+  Update = 'update',
+  Delete = 'delete',
+}
+
+type Subjects = InferSubjects<typeof User> | 'all';
+
+export type AppAbility = Ability<[Action, Subjects]>;
+
+@Injectable()
+export class CaslAbilityFactory {
+  createForUser(user: User) {
+    // eslint-disable-next-line prettier/prettier
+    const { can, build } = new AbilityBuilder<
+      Ability<[Action, Subjects]>
+      // eslint-disable-next-line prettier/prettier
+    >(Ability as AbilityClass<AppAbility>);
+
+    if (user.role === 'admin') {
+      // Read-write access to everything
+      can(Action.Manage, 'all');
+    }
+
+    can(Action.Create, User);
+    can(Action.Read, User, { id: user.id });
+    can(Action.Update, User, { id: user.id });
+    can(Action.Delete, User, { id: user.id });
+
+    return build({
+      detectSubjectType: (item) =>
+        item.constructor as ExtractSubjectType<Subjects>,
+    });
+  }
+}
