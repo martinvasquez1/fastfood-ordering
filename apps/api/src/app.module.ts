930331bd5548a4f304c9361e5d571a99754cb282
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-04-27 14:29:22 -0400

    feat(api): configure app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 
 import { LinksModule } from './links/links.module';
-
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+import typeOrmConfig from './config/type-orm.config';
+
 @Module({
-  imports: [LinksModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(typeOrmConfig()),
+    UsersModule,
+    AuthModule,
+    LinksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: configService.get<string>('ORIGIN'),
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   await app.listen(3002);
 }
 
