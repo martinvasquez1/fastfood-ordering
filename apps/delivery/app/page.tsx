a4030f5ff7b12398db31e6d269c76c5ee4475961
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-04-27 21:50:28 -0400

    chore: add lucide to both web projects

--- apps/delivery/app/page.tsx
+++ apps/delivery/app/page.tsx
@@ -1,12 +1,22 @@
 import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
 
+import { Heart } from 'lucide-react';
+
 export default function Home() {
   return (
     <div>
-      <h1>This is apps/delivery</h1>
+      <h1>This is apps/web</h1>
+
       <Button>Button from UI package</Button>
-      <Card title="Title">Button from UI package</Card>
+      <Button disabled>Button from UI package</Button>
+
+      <Button variant="secondary">Button from UI package</Button>
+      <Button variant="destructive">Button from UI package</Button>
+
+      <Button variant="icon"><Heart size={24} color="red" /></Button>
+
+      <Card title="Title">Lorem</Card>
     </div>
   );
 }
