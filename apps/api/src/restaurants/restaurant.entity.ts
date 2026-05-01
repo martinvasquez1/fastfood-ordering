3d3093eda256cbd069890944c8600534fdb13642
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-04-30 21:49:28 -0400

    feat(api): add restaurant entity

---
+++ apps/api/src/restaurants/restaurant.entity.ts
@@ -0,0 +1,17 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('restaurants')
+export class Restaurant {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "varchar", length: 100 })
+  name: string;
+
+  @Column({ type: "varchar", length: 255 })
+  address: string;
+}
