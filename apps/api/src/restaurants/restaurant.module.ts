0f9941f6a2ca092bac0c705280eda66e90da3eda
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-05-01 21:13:58 -0400

    fix(api): add restaurant stock to module import

--- apps/api/src/restaurants/restaurant.module.ts
+++ apps/api/src/restaurants/restaurant.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Restaurant } from './restaurant.entity';
+import { RestaurantStock } from './restaurant-stock.entity';
 
 import { RestaurantsController } from './restaurant.controller';
 import { RestaurantsService } from './restaurant.service';
 import { RestaurantsRepository } from './restaurant.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant])],
+  imports: [TypeOrmModule.forFeature([Restaurant, RestaurantStock])],
   controllers: [RestaurantsController],
   providers: [RestaurantsService, RestaurantsRepository],
 })
