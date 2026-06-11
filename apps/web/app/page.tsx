02e8b9f71e09fd69cf0c232134df424309a465d6
arjmak <errrgmak35@gmail.com>
2026-06-11 13:19:05 -0400

    Testing Tracking Component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,4 @@
 import OrderTrackingPage from "./order-tracking/OrderTrackingPage"
 export default function Home() {
-<<<<<<< HEAD
   return <OrderTrackingPage/>;
 }
-=======
-  return <CheckoutPage />;
-}
->>>>>>> 71e7acfcf3e8deaa5874bc34aaefd8ef3550290d
