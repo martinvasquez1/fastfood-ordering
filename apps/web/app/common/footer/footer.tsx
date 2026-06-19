07805bfd3a3fe212e47cb0e9f6f2b53b602edde6
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-06-19 16:57:22 -0400

    feat(web): add footer

---
+++ apps/web/app/common/footer/footer.tsx
@@ -0,0 +1,40 @@
+export function footer({ }) {
+    return (
+        <footer className="footer">
+            <ul className="icons">
+                <li className="icon-elem">
+                    <a href="" className="icon">
+                        {/*<ion-icon name="share-social-outline"></ion-icon>*/}
+                    </a>
+                </li>
+
+                <li className="icon-elem">
+                    <a href="" className="icon">
+                        {/*<ion-icon name="share-social-outline"></ion-icon>*/}
+                    </a>
+                </li>
+
+                <li className="icon-elem">
+                    <a href="" className="icon">
+                        {/*<ion-icon name="share-social-outline"></ion-icon>*/}
+                    </a>
+                </li>
+
+
+            </ul>
+            <ul className="menuu">
+                <li className="menu-elem">
+                    <a href="" className="menu-icon"> Menu </a>
+                </li>
+                <li className="menu-elem">
+                    <a href="" className="menu-icon"> Ubicaciones </a>
+                </li>
+                <li className="menu-elem">
+                    <a href="" className="menu-icon"> Contacto </a>
+                </li>
+            </ul>
+            <p className="texto"> ® PAPAPITA </p>
+            <p className="texto"> 2026 | Todos los derechos reservados</p>
+        </footer>
+    )
+}
